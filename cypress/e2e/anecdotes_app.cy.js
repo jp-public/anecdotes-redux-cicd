0534c093ae6a7da522dd648687f443d6acb84055
juhopaakkanen <juho.paakkanen@gmail.com>
2022-07-27 15:24:46 +0300

    Add e2e tests to workflow

--- cypress/e2e/anecdotes_app.cy.js
+++ cypress/e2e/anecdotes_app.cy.js
@@ -1,8 +1,9 @@
 /* eslint-disable jest/expect-expect */
 /* eslint-disable no-undef */
-describe('Pokedex', function () {
+describe('Anecdotes', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3001')
+    cy.wait(3000)
     cy.contains('Anecdotes')
     cy.contains('If it hurts, do it more often')
   })
